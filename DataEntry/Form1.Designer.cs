f0027ca7e74732daed319c5108d9523972bbb7fe
Nawa Kawasaki <n0wa0904@gmail.com>
2017-01-22 07:58:05 -0600

    First commit

---
+++ DataEntry/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace DataEntry
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Submit = new System.Windows.Forms.Button();
+            this.Update = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(46, 22);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 83);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(840, 322);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Submit
+            // 
+            this.Submit.Location = new System.Drawing.Point(287, 18);
+            this.Submit.Name = "Submit";
+            this.Submit.Size = new System.Drawing.Size(89, 24);
+            this.Submit.TabIndex = 2;
+            this.Submit.Text = "Submit";
+            this.Submit.UseVisualStyleBackColor = true;
+            this.Submit.Click += new System.EventHandler(this.Submit_Click);
+            // 
+            // Update
+            // 
+            this.Update.Location = new System.Drawing.Point(720, 432);
+            this.Update.Name = "Update";
+            this.Update.Size = new System.Drawing.Size(132, 34);
+            this.Update.TabIndex = 3;
+            this.Update.Text = "Update";
+            this.Update.UseVisualStyleBackColor = true;
+            this.Update.Click += new System.EventHandler(this.Update_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(876, 478);
+            this.Controls.Add(this.Update);
+            this.Controls.Add(this.Submit);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "frmMain";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Submit;
+        private System.Windows.Forms.Button Update;
+    }
+}
+
---
+++ DataEntry/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataEntry
+{
+    public partial class frmMain : Form
+    {
+        //public string server = System.Configuration.ConfigurationManager.AppSettings["Server"];
+        //public string database =System.Configuration.ConfigurationManager.AppSettings["Database"];
+
+        SqlConnection con = new SqlConnection("Data Source=Lenovo;Initial Catalog=Sample;Integrated Security=true;");
+        //SqlConnection con = new SqlConnection("Data Source=ARCUDB;Initial Catalog=SavvyDBSolarity;Integrated Security=true;");
+
+        SqlCommand cmd;
+        SqlDataAdapter adapt;
+        DataSet ds;
+
+        //public SqlConnection con = new SqlConnection("Server=Lenovo;Database=DataEntry;Trusted_Connection=true");
+        
+        public frmMain()
+        {
+            InitializeComponent();
+            dateTimePicker1.Value = DateTime.Now;
+            //Display();
+        }
+
+ 
+
+        private void Display()
+        {
+            try
+            {
+                con.Open();
+                DataTable dt = new DataTable();
+                adapt = new SqlDataAdapter("SELECT [AccountNumber],[LoanID],[FundedAmount],[LoanTypeDescription],[MarkForDelete] "
+                                           + "FROM [dbo].[loanFundedLIP] "
+                                           + "where [EffectiveDate]= '" + dateTimePicker1.Value.ToString() + "'"
+                                           + "And [MarkForDelete] = '" + 0 + "'"
+                                           , con); // 
+                ds = new System.Data.DataSet();
+                adapt.Fill(ds, "Loans");
+                dataGridView1.DataSource = ds.Tables[0];
+                //adapt.Fill(dt);
+                //dataGridView1.DataSource = dt;
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+            
+        }
+
+        private void Submit_Click(object sender, EventArgs e)
+        {
+            Display();
+        }
+
+        private void Update_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlCommandBuilder cmdbl = new SqlCommandBuilder(adapt);
+                adapt.Update(ds, "Loans");
+                MessageBox.Show( "Update Completed", "Update",MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        
+
+
+        
+
+    }
+}
